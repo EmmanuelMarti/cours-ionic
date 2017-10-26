41f220f18fa970befcd0431ddf4c9ec16f63ac47
MANU-PC\G551JX-DM344T <emmanuel.marti@ynov.com>
2017-10-26 16:38:56 +0200

    Mauvaise variable

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -25,13 +25,13 @@ export class CameraPage {
 	 	// imageData is either a base64 encoded string or a file URI
 	 	// If it's base64:
 	 	this.base64Image = 'data:image/jpeg;base64,' + imageData;
-	 	this.base64ToGallery.base64ToGallery(base64Image, { prefix: '_img' }).then(
+	 	this.base64ToGallery.base64ToGallery(imageData, { prefix: '_img' }).then(
   			res => console.log('Saved image to gallery ', res),
   			err => console.log('Error saving image to gallery ', err)
 		)
 		}, (err) => {
 	 	// Handle error
-	 	
+
 		});
 	}
 	
